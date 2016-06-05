2e56727e6d77539771932290e1f5dce9d594da75
Iamloser <zh602877509@126.com>
2016-06-05 12:24:01 +0800

    A problem for domain model

--- src/ctrl.js
+++ src/ctrl.js
@@ -1,10 +1,3 @@
-myApp.controller('WorldCtrl', ['$scope', function($scope){
-	$scope.population = 7000;
-	$scope.countries = [
-		{name:'France',population:63.1},
-		{name:'UK',population:61.8}
-	];
-	$scope.worldsPercentage = function(countryPopulation){
-		return (countryPopulation / $scope.population) * 100 ;
-	}
-}])
+myApp.controller('HelloCtrl', ['$scope', function($scope){
+
+}]);
